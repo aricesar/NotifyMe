e0200d84e2bb67d870edd5afdeee5250edd8b46f
Gestao-judo <gestaojudo@gmail.com>
2024-04-14 02:26:32 -0300

    Inicializa o projeto NotifyMe com estrutura básica e dependências do Spring Boot

---
+++ src/main/java/br/com/aricesar/NotifyMe/NotifyMeApplication.java
@@ -0,0 +1,13 @@
+package br.com.aricesar.NotifyMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aricesar/NotifyMe/NotifyMeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aricesar.NotifyMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
